d8c7b332b628d8458c8845d16a4b2509dc681b18
rajdeep sahoo <rajdeepsahoo1994@gmail.com>
2022-02-07 16:18:59 +0530

    initial push

---
+++ DesignPattern/src/com/competitive/digitdp/ClassyNumber.java
@@ -0,0 +1,12 @@
+package com.competitive.digitdp;
+
+public class ClassyNumber {
+//	https://codeforces.com/problemset/problem/1036/C
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ DesignPattern/src/com/competitive/digitdp/TotalPalindromicNumers.java
@@ -0,0 +1,12 @@
+package com.competitive.digitdp;
+
+public class TotalPalindromicNumers {
+
+	//https://www.codechef.com/problems/AOPN
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
